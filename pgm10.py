29ab43da3a5a880e5a92fc18efcafff05a38991c
Krish <151437686+Krish-aj@users.noreply.github.com>
2026-06-28 20:52:11 +0530

    Refactor K-Means clustering and update visualizations
    Refactor K-Means clustering implementation and visualization. Updated DataFrame column names for clarity.

--- pgm10.py
+++ pgm10.py
@@ -1,9 +1,3 @@
- PROGRAM 10
-AIM
-
-To develop a Python program to implement K-Means Clustering using the Wisconsin Breast Cancer dataset and to visualize the clustering results using PCA.
-
-
 import matplotlib.pyplot as plt, seaborn as sns, pandas as pd
 from sklearn.datasets import load_breast_cancer
 from sklearn.cluster import KMeans
@@ -14,20 +8,26 @@ from sklearn.metrics import confusion_matrix, classification_report
 X, y = load_breast_cancer(return_X_y=True)
 X = StandardScaler().fit_transform(X)
 
-k = KMeans(2, random_state=42).fit(X)
-y_k = k.labels_
+k = KMeans(2, random_state=42)
+y_k = k.fit_predict(X)
 
 print(confusion_matrix(y, y_k))
 print(classification_report(y, y_k))
 
 p = PCA(2)
-df = pd.DataFrame(p.fit_transform(X), columns=['PC1','PC2'])
-df['C'], df['T'] = y_k, y
+df = pd.DataFrame(p.fit_transform(X), columns=["PC1", "PC2"])
+df["Cluster"] = y_k
+df["Target"] = y
 
-sns.scatterplot(data=df, x='PC1', y='PC2', hue='C');plt.title("k-mean"); plt.show()
-sns.scatterplot(data=df, x='PC1', y='PC2', hue='T');plt.title("cluster"); plt.show()
+sns.scatterplot(data=df, x="PC1", y="PC2", hue="Cluster")
+plt.title("K-Means")
+plt.show()
+sns.scatterplot(data=df, x="PC1", y="PC2", hue="Target")
+plt.title("Cluster")
+plt.show()
 
 c = p.transform(k.cluster_centers_)
-sns.scatterplot(data=df, x='PC1', y='PC2', hue='C')
-plt.scatter(c[:,0], c[:,1], c='red', marker='X'); plt.title("centroid"); plt.show()
+sns.scatterplot(data=df, x="PC1", y="PC2", hue="Cluster")
+plt.scatter(c[:,0], c[:,1], c="red", marker="X")
+plt.title("Centroids")
 plt.show()
